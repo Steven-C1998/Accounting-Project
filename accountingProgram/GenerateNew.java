757d3bf75e29063ee6d1f4ef2241d5f7155cec12
Foo Wai Chong <76924240+Steven-C1998@users.noreply.github.com>
2021-10-26 01:09:58 -0400

    Update GenerateNew.java

--- accountingProgram/GenerateNew.java
+++ accountingProgram/GenerateNew.java
@@ -2,8 +2,9 @@ package accountingProgram;
 
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import java.util.Arrays;
 
-final class GenerateNew {
+final class GenerateNew extends FileKeeper{
 
 	static void printInvoice () throws FileNotFoundException { //prints invoice for each customer
 		int i = 0;
@@ -53,16 +54,16 @@ final class GenerateNew {
 		}
 		System.setOut(originalOut);
 	}
-	
-	static void masterFileNew(String[][] masterArray) throws FileNotFoundException {
+
+	static void masterFileNew() throws FileNotFoundException { //creates new master file
 		PrintStream originalOut = System.out;
 		PrintStream fileOut = new PrintStream("NewMaster.txt");
 		int count = 0;
 		System.setOut(fileOut);	
-		for (int i = 0; i < masterArray.length; i++) {
-			for (int j = 0; j < masterArray[i].length; j++) {
+		for (int i = 0; i < masterFileArray.length; i++) {
+			for (int j = 0; j < masterFileArray[i].length; j++) {
 				count++;
-				System.out.print(masterArray[i][j] + " ");
+				System.out.print(masterFileArray[i][j] + " ");
 				if(count % 3 == 0) {
 					System.out.println("");
 				}
@@ -70,5 +71,166 @@ final class GenerateNew {
 		}
 		System.setOut(originalOut);
 	}
-
+	
+	 //corrects arrays for duplicate entries. Trying to fix errors relating to duplicates found one after another
+	static void correctedArray(String[][] masterArray, String[][] dupeArray, String[][] correctedArray, int dupeLength) {
+		String compareTo ="";
+		String compare ="";
+		String fill = "";
+		String filler = "";
+		String dupeFiller = "";
+		String dupeCompare = "";
+		String dupeCompareTo = "";
+		String dupeFiller2 = "";
+		int j = 0;
+		int counter = 0;
+		int dupeCounter = 0;
+		int nullCheck = 0;
+		int i = 0;
+		int dupeArrayLength =  dupeLength;
+		int m = 0;
+		System.out.println("dupe length " + dupeArrayLength);
+		System.out.println(Arrays.deepToString(dupeArray));
+		for (;i<masterArray.length;) {
+			fill="";
+			for(int p = 0;p<masterArray[i].length;p++) {
+				compare = masterArray[i][p];
+				fill += compare;
+			}
+			for (;j<masterArray.length;) {
+				for(int o = 0;o<dupeArray[j].length;o++) {
+					if (dupeArray[j][o] != null) {
+						compareTo = dupeArray[j][o];
+						filler += compareTo;
+					}
+				}
+				nullCheck++;
+				System.out.println("nullCheck " + nullCheck);
+				System.out.println("filler " +filler);
+				System.out.println("fill " +fill);
+				if(fill.equals(filler) && dupeCounter < masterArray.length -1 ) {
+					for (;m < correctedArray.length;) {
+						dupeFiller = "";
+						for(int a = 0; a < correctedArray[m].length;a++) {
+							dupeCompare = correctedArray[m][a];
+							dupeFiller += dupeCompare; 
+						}
+						for (;j<correctedArray.length;) {
+							if (dupeFiller.equals(fill)) {
+								continue;
+							}
+							else if (!(dupeFiller.equals(fill))  ){
+								if (masterArray[i].length == 3) {
+									String one = masterArray[i][0];
+									String two = masterArray[i][1];
+									String three = masterArray[i][2];
+									System.out.println("Dupe counter " + dupeCounter);
+									System.out.println("master " + masterArray.length);
+									System.out.println("i " + i);
+									System.out.println("fill 2 " +fill);
+									System.out.println("dupefill 2 " + dupeFiller);
+									correctedArray[dupeCounter] = new String[]{one ,two, three};
+									dupeCounter++;
+									filler="";
+									dupeFiller="";
+									fill="";	
+									if (i+dupeArrayLength>=masterArray.length) {
+										System.out.println(Arrays.deepToString(FileKeeper.getCorrectedMasterArray()));
+										return;
+									}
+								} else if (masterArray[i].length == 4) {
+									String one = masterArray[i][0];
+									String two = masterArray[i][1];
+									String three = masterArray[i][2];
+									String four = masterArray[i][3];
+									correctedArray[dupeCounter] = new String[]{one ,two, three, four};
+									dupeCounter++;
+								} else if (masterArray[i].length == 6) {
+									String one = masterArray[i][0];
+									String two = masterArray[i][1];
+									String three = masterArray[i][2];
+									String four = masterArray[i][3];
+									String five = masterArray[i][4];
+									String six = masterArray[i][5];
+									correctedArray[dupeCounter] = new String[]{one ,two, three, four, five, six};
+									dupeCounter++;
+								}
+								counter ++;
+								break;
+							}
+						}
+						if (counter == 1) {
+							break;
+						}
+					}
+					i++;
+					j++;
+					dupeFiller="";
+					dupeFiller2="";	
+					filler="";
+					continue;
+				}else if (!(fill.equals(filler)) && nullCheck-1 < dupeArrayLength) {
+					m++;
+					filler="";
+				} else if(!(fill.equals(filler)) && nullCheck > dupeArrayLength) {
+					if (masterArray[i].length == 3) {
+						String one = masterArray[i][0];
+						String two = masterArray[i][1];
+						String three = masterArray[i][2];
+						correctedArray[dupeCounter] = new String[]{one ,two, three};
+						System.out.println("here");
+						System.out.println(i);
+						System.out.println(filler);
+						System.out.println(fill);
+						System.out.println(Arrays.deepToString(FileKeeper.getCorrectedMasterArray()));
+						System.out.println(dupeFiller2);
+						System.out.println(dupeFiller);
+						dupeCounter++;
+						filler="";
+						dupeFiller="";
+						dupeFiller2="";	
+						i++;
+						m=0;
+						nullCheck = 0;
+						break;
+					} else if (masterArray[i].length == 4) {
+						String one = masterArray[i][0];
+						String two = masterArray[i][1];
+						String three = masterArray[i][2];
+						String four = masterArray[i][3];
+						correctedArray[dupeCounter] = new String[]{one ,two, three, four};
+						dupeCounter++;
+						filler="";
+						dupeFiller="";
+						dupeFiller2="";	
+						i++;
+						j=0;
+						nullCheck = 0;
+						break;
+					} else if (masterArray[i].length == 6) {
+						String one = masterArray[i][0];
+						String two = masterArray[i][1];
+						String three = masterArray[i][2];
+						String four = masterArray[i][3];
+						String five = masterArray[i][4];
+						String six = masterArray[i][5];
+						correctedArray[dupeCounter] = new String[]{one ,two, three, four, five, six};
+						dupeCounter++;
+						filler="";
+						dupeFiller="";
+						dupeFiller2="";	
+						i++;
+						j=0;
+						nullCheck = 0;
+						break;
+					}
+				} else if(dupeCounter > masterArray.length -1) {
+					break;
+				}
+			}
+		}
+	}
+	
 }
+
+
